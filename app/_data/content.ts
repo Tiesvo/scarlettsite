d55bbe0776134c6d9a72230e1b54837ad19efb9c
Ties <ties@vanoldenborgh.com>
2026-03-28 19:18:34 +0100

    first commit

--- app/_data/content.ts
+++ app/_data/content.ts
@@ -3,8 +3,9 @@ export const content = {
   title: "Model & Creator",
   tagline: "Welcome.. find my spicy links here. Will you send me a message babe?",
   ctas: [
+    { label: "F2F", href: "https://f2f.com/lunaa21", brand: "f2f" },
     { label: "Telegram", href: "https://t.me/lunasmit1", brand: "telegram" },
-  ],
+	],
   gallery: Array.from({ length: 19 }).map((_, i) =>
     `/gallery/${String(i + 1).padStart(3, "0")}.jpg`
   ),
